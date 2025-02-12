8d34a2549496e41ef70f0622fbc9838658aaf7f9
HamedMiri72 <hamedmiri72@gmail.com>
2025-02-12 09:25:28 +0000

    showing blogList in admin dashboard

--- src/app/admin/blogList/page.jsx
+++ src/app/admin/blogList/page.jsx
@@ -1,9 +1,63 @@
 'use client'
 
-import React from 'react'
+import BlogTableItem from '@/components/adminComponents/BlogTableItem'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
 export default function page() {
+
+  const [blogs, setBlogs] = useState([]);
+
+
+  const fetchBlogs = async () => {
+    const response = await axios.get("/api/blog");
+    setBlogs(response.data.blogs);
+    console.log(response.data.blogs)
+
+  }
+
+  useEffect(() => {
+    fetchBlogs();
+  }, [])
   return (
-    <div></div>
+    <div className='flex-1 pt-5 px-5 sm:pt-12 sm:pl-16'>
+
+      <h1>All Blogs</h1>
+      <div className='relative h-[80vh] max-w-[850px] overflow-x-auto mt-4 border border-gray-400 scrollbar-hide'>
+
+          <table className='w-full text-sm text-grey-500'>
+
+            <thead className='text-sm text-gray-700 text-left uppercase bg-gray-50 '>
+              <tr>
+                <th scope='col' className='hidden sm:block px-6 py-3'>
+                  Author name
+
+                </th>
+                <th scope='col' className='px-6 py-3'>
+                  Blog title
+
+                </th>
+                <th scope='col' className='px-6 py-3'>
+                  Blog Date
+
+                </th>
+                <th scope='col' className='px-6 py-3'>
+                  Action
+
+                </th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {blogs.map((item, index) => {
+                return <BlogTableItem key={index} mongoId={item._id} title={item.title} date={item.date} />
+              })}
+              
+            </tbody>
+
+          </table>
+      </div>
+
+    </div>
   )
 }
